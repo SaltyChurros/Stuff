7474018551065505e6ca75d8697d71e69cebedca
SaltyChurros <99356381+SaltyChurros@users.noreply.github.com>
2022-02-15 11:55:47 -0800

    Update ChallengeC.rb

--- ChallengeC.rb
+++ ChallengeC.rb
@@ -1,1 +1,30 @@
-
+use_bpm 160
+use_synth :saw
+define :stranger do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+end
+  with_fx :slicer do
+    play :stranger
+  end
+  with_fx :whammy do
+    play :stranger
+  end
+  with_fx :bitcrusher do
+    play :stranger
+  end
+end
